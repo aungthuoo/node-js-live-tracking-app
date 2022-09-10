ef1e34d88449f13f0855a706d56b7724ef1d1f0d
Ko Aung Thu Oo <koaungthuoo@gmail.com>
2022-09-10 14:33:27 +0630

    Merge mobile tracking

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+//const http = require('http').createServer()
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const http = require('http').Server(app);
+const socketIO = require('socket.io')(http)
 const axios = require("axios");
 
-
-
-var redis = require('redis');
+const redis = require('redis');
 const { DefaultDeserializer } = require('v8');
-var redisSubscriber = redis.createClient();
-var redisPublisher = redis.createClient();
-var client = redis.createClient();
+const redisSubscriber = redis.createClient();
+const redisPublisher = redis.createClient();
+const client = redis.createClient();
 
 
 let UserModel = require('./models/user')
@@ -21,91 +20,132 @@ var routes = require('./routes');
 const db = require('./db');
 
 app.set('view engine', 'ejs');
-// Express Middleware
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
 
+
+
+
+socketIO.on('connection', function (client) {
+  console.log('Connected...', client.id);
+
+//listens for new messages coming in
+  client.on('message', function name(data) {
+    console.log(data);
+    socketIO.emit('message', data);
+  })
+
+
+
+  client.on('lastKnownLocation', function name(data) {
+    debugger; 
+    let locationModel = new LocationModel({
+        //id : data.User.id, 
+        user_id : data.User.id, 
+        name: data.User.name, 
+        latitude : data.Coordinate.Latitude,
+        longitude : data.Coordinate.Longitude, 
+        created_at : new Date()
+    }); 
+    locationModel.save()
+        .then(doc => {
+            console.log(doc)
+        })
+        .catch(err => {
+            console.error(err)
+        })
+    
+
+    var location = JSON.stringify(data);
+    redisPublisher.publish('locationUpdateABC', location);
+});
+
+
+//listens when a user is disconnected from the server
+  client.on('disconnect', function () {
+    console.log('Disconnected...', client.id);
+  })
+
+//listens when there's an error detected and logs the error on the console
+  client.on('error', function (err) {
+    console.log('Error detected', client.id);
+    console.log(err);
+  })
+})
+
+
+
+
+
+
 // index page
 app.get('/', function(req, res) {
-    var mascots = [
-      { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
-      { name: 'Tux', organization: "Linux", birth_year: 1996},
-      { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
-    ];
-    var tagline = "No programming concept is complete without a cute animal mascot.";
-  
-    res.render('pages/index', {
-      mascots: mascots,
-      tagline: tagline
-    });
+  var mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+    { name: 'Tux', organization: "Linux", birth_year: 1996},
+    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+  ];
+  var tagline = "No programming concept is complete without a cute animal mascot.";
+
+  res.render('pages/index', {
+    mascots: mascots,
+    tagline: tagline
+  });
 });
+
 
 // Render Main HTML file
 app.get('/maps', function (req, res) {
-    redisSubscriber.subscribe('locationUpdateABC');
-    res.render('pages/maps', {
-        root: __dirname
-    });
+  redisSubscriber.subscribe('locationUpdateABC');
+  res.render('pages/maps', {
+      root: __dirname
+  });
 });
 
 //Serve a Publisher HTML
 app.get('/publish', function (req, res) {
-    res.sendFile('views/publisher.html', {
-        root: __dirname
-    });
+  res.sendFile('views/publisher.html', {
+      root: __dirname
+  });
 });
 
 //Serve a Publisher HTML
 app.get('/history', function (req, res) {
-    res.render('pages/history', {
-        root: __dirname
-    });
+  res.render('pages/history', {
+      root: __dirname
+  });
 });
 
 
 app.get("/test_cache", (req, res) => {
-    const searchTerm = req.query.search;
-    try {
-        client.get(searchTerm, async (err, jobs) => {
-            if (err) throw err;
-    
-            if (jobs) {
-                res.status(200).send({
-                    jobs: JSON.parse(jobs),
-                    message: "data retrieved from the cache"
-                });
-            }
-            else {
-                //const jobs = await axios.get(`https://jobs.github.com/positions.json?search=${searchTerm}`);
-                const jobs = await axios.get(`https://jsonplaceholder.typicode.com/posts`);
-                client.setex(searchTerm, 600, JSON.stringify(jobs.data));
-                res.status(200).send({
-                    jobs: jobs.data,
-                    message: "cache miss"
-                });
-            }
-        });
-    } catch(err) {
-        res.status(500).send({message: err.message});
-    }
+  const searchTerm = req.query.search;
+  try {
+      client.get(searchTerm, async (err, jobs) => {
+          if (err) throw err;
+  
+          if (jobs) {
+              res.status(200).send({
+                  jobs: JSON.parse(jobs),
+                  message: "data retrieved from the cache"
+              });
+          }
+          else {
+              //const jobs = await axios.get(`https://jobs.github.com/positions.json?search=${searchTerm}`);
+              const jobs = await axios.get(`https://jsonplaceholder.typicode.com/posts`);
+              client.setex(searchTerm, 600, JSON.stringify(jobs.data));
+              res.status(200).send({
+                  jobs: jobs.data,
+                  message: "cache miss"
+              });
+          }
+      });
+  } catch(err) {
+      res.status(500).send({message: err.message});
+  }
 });
-
-//https://stackoverflow.com/questions/59443078/how-can-i-add-an-array-to-hash-with-hmset-in-redis
-/*
-You can add it as another field in the same hash key, but stringify the array.
-
-await redis.hset(`role-${roleId}`, 'mandatories', JSON.stringify(mandatories))
-This way you can still HGETALL the whole role object data. But then, you have to manage the array of IDs as a whole.
-
-Or, you can flatten out the array to a list, set, or sorted set. For example, to add as set, you can do:
-
-await redis.sadd(`role-${roleId}-mandatories`, mandatories)
-Note we are adding '-mandatories' to the key name. Here you are passing the array to the node.js redis sadd function. It will add each array item as a member in the set. This allows you to manipulate the set of mandatories directly (SPOP, SREM, SISMEMBER, etc).
-*/
-
 
 app.get('/share', function (req, res) {
    debugger; 
@@ -114,6 +154,7 @@ app.get('/share', function (req, res) {
     var latitude = req.query.lat ?? 0.0 ;
     var longitude = req.query.long ?? 0.0 ;
     
+
 
     res.render('pages/share', {
         root: __dirname,
@@ -125,12 +166,12 @@ app.get('/share', function (req, res) {
 });
 
 app.get('/tracking', function (req, res) {
-    debugger; 
      var id = req.query.id ?? 0 ;
      var name = req.query.name ?? "" ;
      var latitude = req.query.lat ?? 0.0 ;
      var longitude = req.query.long ?? 0.0 ;
      var status = req.query.status ?? 1 ;
+
 
 
      res.render('pages/tracking', {
@@ -143,74 +184,13 @@ app.get('/tracking', function (req, res) {
      });
 });
 
-app.use('/', routes);
 
-http.listen(process.env.PORT || 3000, function () {
-    console.log('Server Started. Listening on :3000');
+
+
+
+
+var port = process.env.PORT || 3000;
+http.listen(port, function (err) {
+  if (err) console.log(err);
+  console.log('Listening on port', port);
 });
-
-
-
-
-
-
- 
-
-
-
-
-
-redisSubscriber.on('subscribe', function (channel, count) {
-    console.log('client subscribed to ' + channel + ', ' + count + ' total subscriptions');
-});
-redisSubscriber.on('message', function (channel, message) {
-    console.log('step 3'); 
-    //console.log('client channel ' + channel + ': ' + message);
-    io.emit('locationUpdate', message);
-});
-
-
-io.on('connection', function (socket) {
-    console.log('socket created');
-    let previousId;
-    const safeJoin = currentId => {
-        socket.leave(previousId);
-        socket.join(currentId);
-        previousId = currentId;
-      };
-    socket.on('disconnect', function() {
-      console.log('Got disconnect!');
-    });
-
-
-    socket.on('message', function name(data) {
-        console.log(data);
-        io.emit('message', data);
-    }); 
-
-
-    socket.on('lastKnownLocation', function (data) {
-        debugger; 
-        let locationModel = new LocationModel({
-            //id : data.User.id, 
-            user_id : data.User.id, 
-            name: data.User.name, 
-            latitude : data.Coordinate.Latitude,
-            longitude : data.Coordinate.Longitude, 
-            created_at : new Date()
-        }); 
-        locationModel.save()
-            .then(doc => {
-                console.log(doc)
-            })
-            .catch(err => {
-                console.error(err)
-            })
-        
-
-        var location = JSON.stringify(data);
-        redisPublisher.publish('locationUpdateABC', location);
-    });
-});
-
-
--- readme.md
+++ readme.md
@@ -10,3 +10,19 @@ http://0.0.0.0:3000/tracking?id=4&name=BMK&lat=16.82755509381635&long=96.1737135
 
 
 http://0.0.0.0:3000/history
+
+
+
+
+//https://stackoverflow.com/questions/59443078/how-can-i-add-an-array-to-hash-with-hmset-in-redis
+/*
+You can add it as another field in the same hash key, but stringify the array.
+
+await redis.hset(`role-${roleId}`, 'mandatories', JSON.stringify(mandatories))
+This way you can still HGETALL the whole role object data. But then, you have to manage the array of IDs as a whole.
+
+Or, you can flatten out the array to a list, set, or sorted set. For example, to add as set, you can do:
+
+await redis.sadd(`role-${roleId}-mandatories`, mandatories)
+Note we are adding '-mandatories' to the key name. Here you are passing the array to the node.js redis sadd function. It will add each array item as a member in the set. This allows you to manipulate the set of mandatories directly (SPOP, SREM, SISMEMBER, etc).
+*/
