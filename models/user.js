3df692766dbfc548e9b37c939816934e7a113906
Ko Aung Thu Oo <koaungthuoo@gmail.com>
2022-09-16 15:54:40 +0630

    change ui

--- models/user.js
+++ models/user.js
@@ -1,4 +1,11 @@
 let mongoose = require('mongoose')
+// Database Connection
+mongoose.connect('mongodb://127.0.0.1:27017/live_tracking',{
+    useNewUrlParser: true,
+    //useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
 mongoose.set('debug', true);
 let validator = require('validator')
 
