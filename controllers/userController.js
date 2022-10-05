654b6c352be00db8b4aaa33680ef00837bc004eb
Ko Aung Thu Oo <koaungthuoo@gmail.com>
2022-10-05 13:45:45 +0630

    change timezone

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,6 +41,36 @@ exports.update = async (item) => {
         // do something with the document
     });
 
+
+
+// To Test 
+var updatedAt = moment().tz("Asia/Yangon").format()
+//const updatedAt = moment.tz(Date.now(), "Asia/Rangoon");
+// res.status(200).json( { "status" : true, "data" : updatedAt });
+
+var _id = 25924; 
+var query = {id : _id},
+update = { 
+    name : "name123", 
+    id : _id, 
+    latitude : 16.001, 
+    longitude : 96.001, 
+    order_count : 31, 
+    active_at : moment.utc().toDate(), 
+    updated_at :  updatedAt
+    //updated_at: new Date()
+    //expire: new Date() 
+},
+options = { upsert: true, new: true, setDefaultsOnInsert: true };
+
+// Find the document
+UserModel.findOneAndUpdate(query, update, options, function(error, result) {
+    if (error) return;
+    // do something with the document
+});
+
+
+
 }
 
 
