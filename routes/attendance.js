b9da3be5086faba09a98ab48ba186ec28564b4f0
Ko Aung Thu Oo <koaungthuoo@gmail.com>
2022-10-22 14:43:19 +0630

    Add date filter

--- routes/attendance.js
+++ routes/attendance.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const {index, active, save, update, daily, updateWorkingHourInterval, 
-        showTestRecord, updateTestRecord, workingHour, todayShiftInfo
+        showTestRecord, updateTestRecord, workingHour, todayShiftInfo, workingHours
         
     } = require('../controllers/attendanceController');
 
@@ -13,6 +13,7 @@ router.post('/update-working-hour-interval', updateWorkingHourInterval);
 router.get('/show-test-record', showTestRecord);
 router.post('/update-test-record', updateTestRecord);
 router.get('/working-hour', workingHour);
+router.get('/working-hours', workingHours);
 
 router.post('/today-shift-info', todayShiftInfo);
 
